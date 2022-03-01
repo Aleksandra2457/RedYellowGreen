e701253c14220c5118a5a986842e46128dcd0f25
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-03-01 18:27:20 +0300

    Initial Commit

---
+++ RedYellowGreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedYellowGreen
+//
+//  Created by Александра Лесовская on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
